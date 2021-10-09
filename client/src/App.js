8a3f3ccb3f1523f04cd98ba0a734e8a6ed8d31b0
bmaner <bmanerdaniel@gmail.com>
2021-10-09 22:17:01 +0900

    Add Header, Carousel component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Header from "../src/components/common/Header";
+import Carousel from "./components/slidersection/Carousel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Carousel />
+      <Switch> </Switch>
     </div>
   );
 }
---
+++ client/src/components/common/Menu.js
@@ -0,0 +1,5 @@
+function Menu({ name, position }) {
+  return <div class="menu">{name}</div>;
+}
+
+export default Menu;
